45ed4e3961737b43ed2e8a182b0bd5c642b51540
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2021-07-16 16:29:59 +0700

    Delete file

--- Updater.sh
+++ Updater.sh
@@ -2,7 +2,14 @@
 echo "
 rm -rf $PREFIX/share/quickbms
 rm -rf thepakv2.0
+rm -rf $HOME/thepakv2/etc
 rm date.expired
+
+ echo "PLEASE CHECK CHANNEL THEUNKNOWNNAME"
+ echo " they will update script"
+ sleep 1
+ exit
+
 " >../date.expired
 
 cd ..
